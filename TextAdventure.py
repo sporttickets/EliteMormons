12513b8b3983308a29f5d7f16656e0c75308eb4f
sporttickets <kincadekade@gmail.com>
2015-04-19 14:08:03 -0600

    Update and rename test.py to TextAdventure.py

---
+++ TextAdventure.py
@@ -0,0 +1,9 @@
+def game():
+	print ("Hello")
+	name = raw_input("What is your name")
+	print ("Welcome, "+name+"")
+	print (""+name+" You just woke up. Im suprised to see that you can rember your name hi im Alfred You look like you hit your head hard on that fall")
+	print ("Alright i will be leaving you good luck "+name+"")
+	import time
+	time.sleep(5)
+	
--- test.py
+++
@@ -1,9 +0,0 @@
-def game():
-	print ("You've just woke up and you don't rember anything")
-	print ("Do you take the door on the left or the door on the right")
-	answer = raw_input("Type left or Right and hit 'Enter'").lower()
-	if answer == "left":
-		print ("This door leads to an air plane with chianes men playing sum sick games m8")
-	elif answer =="right":
-		print ("This is the is a pub")	
-game()
