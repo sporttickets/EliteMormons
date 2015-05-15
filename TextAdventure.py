2e7660b1a3bef29ed7ae8ba62f1847356caddce3
sporttickets <kincadekade@gmail.com>
2015-05-15 14:42:13 -0600

    new game

--- TextAdventure.py
+++ TextAdventure.py
@@ -1,56 +1,70 @@
 import sys
 from time import sleep
+
 def loading(str):
     import sys
     from time import sleep
 
     for char in str:
-        sleep(.02)
+        sleep(0.0)
         sys.stdout.write(char)
         sys.stdout.flush()
-
 
 def typewrite(str):
     import sys
     from time import sleep
 
     for char in str:
-        sleep(0.02)
+        sleep(0.03)
         sys.stdout.write(char)
         sys.stdout.flush()
 
+def text():
+    loading("Loading........................................")
+    import time
+    typewrite("\nNARRATOR:\nYou just woke up and you don't know where you are or your name...")
 
-loading("Loading.................")
-import time
-time.sleep(0)
-typewrite("\nThis game was created by KinCade McCown\n")
-time.sleep(1)
+    time.sleep(3)
 
-typewrite("You just woke up... Hi im alfred i just saw you fall.. whats your name\n")
-name= input("Whats your name\n")
-typewrite(" Hi {} \n well i have to leave soon so good luck\n".format(name))
-typewrite(" {} you see s a gaint tree with two doors a left and a right door ".format(name))
-typewrite("\n Do you take the left door or the right door")
-option1 = input("Left or Right\n").lower()
-if option1 == "left":
-    typewrite ("You go to the door and open it slowly and you smell and odor of wizard ...You see people all around but one in particular is looking at you with a face of happines")
-    typewrite("You walk to the wizard but then a old handsome i MEAN HANDSOME !MAN offers a DELSIH I MEAN DELISH looking of coco do you take the coco or do you talk to the old wizard who is hot.")
-option3 = input("Drink or talk\n").lower()
-if option3 == "drink":
-    typewrite("You grab that delish looking coco and you drink it but one problem its made for bronies and yoru not a bronie \n anything with a bronie on it is evil savege and bad sir bad")
-    typewrite("\n you pass out and you wake up in a house that is not cool.")
-elif option1 == "right":
-    typewrite("You walk into a room and you are confronted by whirlwin particular smith WPS for short.. he says {} what are u doing here?".format(name))
-    tyepwrite("\n You should be doing your quest of EPIC PROPORTIONS")
-    typewrite("{} UHH what quest?: Dude are u cearl you need to be slaying some draongs right now so i can make some realy money man\n")
-option2 = input("Do the quest or eat some cake?\n").lower()
-if option2 =="quest":
-    typewrite("you follow young phillips to a carrage")
-elif option2 == "eat some cake":
-    typewrite("\n You sit down and enjoy some cake")
+    typewrite("\nFredrick:\nAre you alright you just fell from that tree ...\n")
+
+    time.sleep(2)
+
+    typewrite("Whats your name?")
+
+    time.sleep(3)
+
+    typewrite("\nUhhh")
+
+    time.sleep(3)
+
+    typewrite("\nthinking.....")
+
+    time.sleep(3)
+
+    typewrite("\nI don't know")
+
+    time.sleep(3)
+
+    typewrite("\nok ill name you Your name will bee......")
+
+    time.sleep(3)
+    name=raw_input("\nWhat will he name you\n")
+
+    time.sleep(3)
+
+    typewrite("\nYour name is {}".format(name))
 
 
+    time.sleep(3)
 
-else: 
-    typewrite("Not an optin")
+    typewrite("\nYou notice the tree you fell from has two doors on it a left and a right door \nthey both lead to a tavern\n")
+
+text()
+q3 = raw_input("Left or Right\n").lower()
+if "left":
+    typewrite("You walk into a smelly tavern.\nThe door locks behind you.")
+elif "right":
+    typewrite("hihi")
+
 
