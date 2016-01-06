de14c2a97e3201cecd94da5d22b776c2847ed92e
Kincade Mccown <kincademccown@Alveyworld-18.local>
2016-01-06 10:17:23 -0700

    Madlib

--- TextAdventure.py
+++ TextAdventure.py
@@ -18,53 +18,53 @@ def typewrite(str):
         sleep(0.03)
         sys.stdout.write(char)
         sys.stdout.flush()
-
 def text():
     loading("Loading........................................")
     import time
     typewrite("\nNARRATOR:\nYou just woke up and you don't know where you are or your name...")
 
-    time.sleep(3)
+
 
     typewrite("\nFredrick:\nAre you alright you just fell from that tree ...\n")
 
-    time.sleep(2)
 
     typewrite("Whats your name?")
 
-    time.sleep(3)
 
     typewrite("\nUhhh")
 
-    time.sleep(3)
+   
 
-    typewrite("\nthinking.....")
-
-    time.sleep(3)
 
     typewrite("\nI don't know")
 
-    time.sleep(3)
+    
 
     typewrite("\nok ill name you Your name will bee......")
 
-    time.sleep(3)
+    time.sleep(1)
     name=raw_input("\nWhat will he name you\n")
 
-    time.sleep(3)
+    
 
     typewrite("\nYour name is {}".format(name))
 
 
-    time.sleep(3)
+
 
     typewrite("\nYou notice the tree you fell from has two doors on it a left and a right door \nthey both lead to a tavern\n")
 
 text()
 q3 = raw_input("Left or Right\n").lower()
 if "left":
+    return True
     typewrite("You walk into a smelly tavern.\nThe door locks behind you.")
 elif "right":
-    typewrite("hihi")
+    return False
+    typewrite("walk into the rigth door")
+else:
+    print "that is not an option"
+
+
 
 
