b790a9b8c029a6542391f08aac646f6f5f52e236
Kincade Mccown <kincademccown@Alveyworld-18.local>
2016-01-08 09:35:15 -0700

    Game

---
+++ madlib21.py
@@ -0,0 +1,16 @@
+#mad lib
+adj1 = raw_input("Enter And adjetive: ")
+adj2 = raw_input("Please Enter Another Adjective: ")
+bird1 = raw_input("Type of Bird ")
+room = raw_input("Type of a room in a house ")
+verb1 = raw_input("Type in a verb ")
+verb2 = raw_input("Type in a verb ")
+ret1 = raw_input("Type in a relvitevs name ")
+noun1 = raw_input("Type in a noun ")
+liq1 = raw_input("type in a liquid ")
+verbing = raw_input("type a verb ending in ing ")
+body = raw_input("Name a body part")
+pluarn = raw_input("pluar noun ")
+noun2 = raw_input("noun ")
+print "It was a " + adj1 + ", Cold November Day\n"
+print "woke up to the" + adj2 + "smell of" + bird1 + "\nroasting in the" + room + "downstaris. I\n" + verb1 + ""
